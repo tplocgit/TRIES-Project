dfbb1f0b2264d01f4fbf4d268fddcafc179ac8e7
Lộc Trần Phước <giongnhuntlf@gmail.com>
2019-12-23 20:30:34 +0700

    FEATURES: Injsertion, Dellocation and Read datas from file

--- TRIES/TRIES/TRIES.cpp
+++ TRIES/TRIES/TRIES.cpp
@@ -1,53 +1,52 @@
 #include<iostream>
 #include<string>
 #include<fstream>
-
+#include<vector>
 using namespace std;
 
 #define ALPHABET_CHARS 26
-
-int8_t getIndex(char c);
+#define MAX_LENGTH 10
 
 // Each node contains hash table with hash function h(k) = k - 97;
-struct TRIES_NODE {
-	TRIES_NODE* m_nodes[ALPHABET_CHARS];
+//--------------------------------------------------------------------
+struct NODE {
+	NODE* m_nodes[ALPHABET_CHARS];
 	bool m_endWord;
-	void Deallocate();
-	TRIES_NODE*& operator[](int index);
-	void setFlag(bool flag);
-	void create();
 };
 
-struct TRIES_TREE {
-	TRIES_NODE* p_root;
-	void Insert(string word, int depth, TRIES_NODE* cur);
-	void CreateWord(char* chars, int n);
-	void create(string file_name);
-};
+int Tree_getIndex(char c);
 
+NODE* Tree_createNODE();
+
+void Tree_InsertWord(NODE*& p_root, string word);
+
+NODE* Tree_ReadFromFile(string path);
+
+void Tree_Deallocate(NODE*& p_root);
+
+vector<string> Tree_GenerateWord(NODE* p_root, char* chars, int n);
+//--------------------------------------------------------------------
 
 int main() {
-	TRIES_TREE Root;
-	Root.create("Test.txt");
-	Root.p_root->Deallocate();
-	cout << "cc\n";
+	NODE* p_root = Tree_ReadFromFile("test.txt");
+	Tree_Deallocate(p_root);
 	system("pause");
 	return 0;
 }
 
-void TRIES_NODE::Deallocate() {
-	if (!this)
+void Tree_Deallocate(NODE*& p_root) {
+	if (!p_root)
 		return;
 	for (int i = 0; i < ALPHABET_CHARS; i++) {
-		if (this) {
-			(*this)[i]->Deallocate();
-			delete[] this;
+		if (p_root->m_nodes[i]) {
+			Tree_Deallocate(p_root->m_nodes[i]);
+			delete[] p_root->m_nodes[i];
 		}
 	}
 }
 
-int8_t getIndex(char c) {
-	int8_t res;
+int Tree_getIndex(char c) {
+	int res;
 	if ('a' <= c && c <= 'z')
 		res = c - 'a';
 	else
@@ -55,56 +54,40 @@ int8_t getIndex(char c) {
 	return res;
 }
 
-TRIES_NODE*& TRIES_NODE::operator[](int index) {
-	return this->m_nodes[index];
-}
-
-void TRIES_NODE::setFlag(bool flag) {
-	this->m_endWord = flag;
-}
-
-void TRIES_NODE::create() {
+NODE* Tree_createNODE() {
+	NODE* p_node = new NODE;
 	for (int i = 0; i < ALPHABET_CHARS; i++)
-		this->m_nodes[i] = nullptr;
-	this->m_endWord = true;
+		p_node->m_nodes[i] = nullptr;
+	p_node->m_endWord = false;
+	return p_node;
 }
 
-void TRIES_TREE::Insert(string word, int depth, TRIES_NODE* cur) {
-	cur = (*cur)[int(getIndex(word.front()))];
-	if (cur) {
-		Insert(word.substr(1, word.size() - 1), depth + 1, cur);
-		cur->setFlag(false);
+void Tree_InsertWord(NODE*& p_root, string word) {
+	NODE* p_cur = p_root;
+	for (auto it = word.begin(); it != word.end(); ++it) {
+		int index = Tree_getIndex(*it);
+		if (!p_cur->m_nodes[index])
+			p_cur->m_nodes[index] = Tree_createNODE();
+		p_cur = p_cur->m_nodes[index];
 	}
-	else {
-		if (word.end() - word.begin() == 1) {
-			cur = new TRIES_NODE;
-			cur->create();
-		}
-		else {
-			cur = new TRIES_NODE;
-			cur->create();
-			Insert(word.substr(1, word.size() - 1), depth + 1, cur);
-		}
-	}
+	p_cur->m_endWord = true;
 }
 
-void TRIES_TREE::CreateWord(char* chars, int n) {
-
-}
-
-void TRIES_TREE::create(string file_name) {
+NODE* Tree_ReadFromFile(string path) {
 	ifstream fin;
-	fin.open(file_name);
+	NODE* p_root = nullptr;
+	fin.open(path);
 	if (!fin.is_open())
 		cout << "Error XXX: File not found.\n";
 	else {
-		this->p_root = new TRIES_NODE;
-		this->p_root->create();
+		p_root = Tree_createNODE();
 		string word;
 		while (!fin.eof()) {
-			fin >> word;
-			this->Insert(word, 0, this->p_root);
+			std::getline(fin, word, '\n');
+			if (word.length() >= 3)
+				Tree_InsertWord(p_root, word);
 		}
 		fin.close();
 	}
+	return p_root;
 }
